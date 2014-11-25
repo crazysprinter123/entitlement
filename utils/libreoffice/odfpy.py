ae40e9d26e1f6a943422cdc56e949ea89f85d2fc
sgao <sgao@redhat.com>
2014-11-25 15:02:39 +0800

    ahhhh

--- utils/libreoffice/odfpy.py
+++ utils/libreoffice/odfpy.py
@@ -18,22 +18,23 @@ class ODSReader:
         # for each row
         for row in rows:
             cells = row.getElementsByType(TableCell)
-            textContent = ""
             # for each cell
             for cell in cells:
+                textContent = ""
                 # repeated value?
                 repeat = cell.getAttribute("numbercolumnsrepeated")
                 if(not repeat):
                     repeat = 1
                 ps = cell.getElementsByType(P)
-                # for each text node
                 for p in ps:
                     for n in p.childNodes:
-                        textContent = unicode(n.data)
+                        if (n.nodeType == 3):
+                            textContent = unicode(n.data)
                 if(textContent):
                     if(textContent[0] != "#"):  # ignore comments cells
                         for rr in range(int(repeat)):  # repeated cells
-                            print textContent
+                            if textContent != "":
+                                print textContent
 
 if __name__ == "__main__":
     ODSReader("test.ods")
