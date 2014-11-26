23a7ae47ac0c0c0d55dbfe66acbf65883584ed3e
qianzhan <baiyueliang@yeah.net>
2014-11-26 05:58:35 -0500

    need debug subprocess function

--- testcases/rhsm/rhsm_level_1/tc_ID284088_register_with_env_should_not_be_supported_in_sam.py
+++ testcases/rhsm/rhsm_level_1/tc_ID284088_register_with_env_should_not_be_supported_in_sam.py
@@ -1,30 +1,29 @@
-import sys, os, subprocess, commands, random, re
-import logging
-from autotest_lib.client.common_lib import error
-from autotest_lib.client.bin import utils
-from autotest_lib.client.virt import virt_test_utils, virt_utils
-from autotest_lib.client.tests.kvm.tests.ent_utils import ent_utils as eu
-from autotest_lib.client.tests.kvm.tests.ent_env import ent_env as ee
+from utils import *
+from testcases.rhsm.rhsmbase import RHSMBase
+from testcases.rhsm.rhsmconstants import RHSMConstants
+from utils.exception.failexception import FailException
 
-def run_tc_ID284088_register_with_env_should_not_be_supported_in_sam(test, params, env):
+class tc_ID284088_register_with_env_should_not_be_supported_in_sam(RHSMBase):
+    def test_run(self):
+        case_name = self.__class__.__name__
+        logger.info("========== Begin of Running Test Case %s ==========" % case_name)
+        try:
+            username = RHSMConstants().get_constant("username")
+            password = RHSMConstants().get_constant("password")
+            org = 'ACME_Corporation'
+            cmd = "subscription-manager register --username=%s --password=%s --org=%s --env=Library"%(username, password, org)
+            (ret, output) = self.runcmd(cmd, "register with environments")
+            if ret != 0 and "Error: Server does not support environments." in output:
+                logging.info("It's successful to verify that register_with_env_should_not_be_supported_in_sam")
+            else:
+                raise FailException("Test Failed - failed to verify that register_with_env_should_not_be_supported_in_sam")
+            self.assert_(True, case_name)
+        except Exception, e:
+            logger.error(str(e))
+            self.assert_(False, case_name)
+        finally:
+            self.restore_environment()
+            logger.info("=========== End of Running Test Case: %s ===========" % case_name)
 
-	session, vm = eu().init_session_vm(params, env)
-	logging.info("=========== Begin of Running Test Case: %s ===========" % __name__)
-
-	try:
-		try:
-			username = ee().get_env(params)["username"]
-			password = ee().get_env(params)["password"]
-			org = 'ACME_Corporation'
-			cmd = "subscription-manager register --username=%s --password=%s --org=%s --env=Library"%(username, password, org)
-			(ret, output) = eu().runcmd(session, cmd, "register with environments")
-			if ret != 0 and "Error: Server does not support environments." in output:
-				logging.info("It's successful to verify that register_with_env_should_not_be_supported_in_sam")
-			else:
-				raise error.TestFail("Test Failed - failed to verify that register_with_env_should_not_be_supported_in_sam")
-		except Exception, e:
-			logging.error(str(e))
-			raise error.TestFail("Test Failed - error happened when register_with_env:" + str(e))
-	finally:
-		logging.info("=========== End of Running Test Case: %s ===========" % __name__)
-
+if __name__ == "__main__":
+    unittest.main()
