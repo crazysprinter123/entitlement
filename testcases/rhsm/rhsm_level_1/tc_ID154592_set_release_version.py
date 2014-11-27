b250bc54a186de47a9c94e1be1c349478b6df15f
qianzhan <baiyueliang@yeah.net>
2014-11-27 03:35:43 -0500

    154591 154592

--- testcases/rhsm/rhsm_level_1/tc_ID154592_set_release_version.py
+++ testcases/rhsm/rhsm_level_1/tc_ID154592_set_release_version.py
@@ -1,58 +1,46 @@
-import sys, os, subprocess, commands, random
-import logging
-from autotest_lib.client.common_lib import error
-from autotest_lib.client.bin import utils
-from autotest_lib.client.virt import virt_test_utils, virt_utils
-from autotest_lib.client.tests.kvm.tests.ent_utils import ent_utils as eu
-from autotest_lib.client.tests.kvm.tests.ent_env import ent_env as ee
+from utils import *
+from testcases.rhsm.rhsmbase import RHSMBase
+from testcases.rhsm.rhsmconstants import RHSMConstants
+from utils.exception.failexception import FailException
 
-def run_tc_ID154592_set_release_version(test, params, env):
-
-	session,vm=eu().init_session_vm(params,env)
-	logging.info("=========== Begin of Running Test Case: %s ==========="%__name__)
-
+class tc_ID154592_set_release_version(RHSMBase):
+    def test_run(self):
+        case_name = self.__class__.__name__
+        logger.info("========== Begin of Running Test Case %s ==========" % case_name)
         #register to server
-	username=ee().get_env(params)["username"]
-	password=ee().get_env(params)["password"]
-	eu().sub_register(session,username,password)
-
-	try:   
-		#get env variables
-		productid=ee().get_env(params)["productid"]
-		autosubprod=ee().get_env(params)["autosubprod"]
-
-		#auto subscribe to a pool
-		eu().sub_autosubscribe(session, autosubprod)
-
-		#set releases
-		guestname=params.get("guest_name")
-		currentversion=eu().sub_getcurrentversion(session,guestname)
-		cmd="subscription-manager release --set=%s"%(currentversion)
-		(ret,output)=eu().runcmd(session,cmd,"set release version")
-		
-
-	        if ret == 0 and "Release set to: %s"%(currentversion) in output:      
-           		logging.info("It's successful to set release version.")	
+        username=RHSMConstants().get_constant("username")
+        password=RHSMConstants().get_constant("password")
+        self.sub_register(username,password)
+        try:
+            autosubprod=RHSMConstants().get_constant("autosubprod")
+            self.sub_autosubscribe(autosubprod)
+            #set releases
+            currentversion=self.sub_getcurrentversion()
+            cmd="subscription-manager release --set=%s"%(currentversion)
+            (ret,output)=self.runcmd(cmd,"set release version")
+            if ret == 0 and "Release set to: %s"%(currentversion) in output:
+                logger.info("It's successful to set release version.")	
+            else:
+                raise FailException("Test Failed - Failed to set release version.")
+            #check the version in repo list
+            cmd = "subscription-manager repos --list|grep 'Repo Url'|grep %s"%currentversion
+            cmd510 = "subscription-manager repos --list|grep 'Repo URL'|grep %s"%currentversion
+            (ret,output)=self.runcmd(cmd,"list available repos")
+            if ret == 0:
+                logger.info("It's successful to display the set release version.")
+            else:
+                (ret,output)=self.runcmd(cmd510,"list available repos")
+                if ret == 0:
+                    logger.info("It's successful to display the set release version.")
                 else:
-        	        raise error.TestFail("Test Failed - Failed to set release version.")
+                    raise FailException("Test Failed - Failed to display the set release version.")
+            self.assert_(True, case_name)
+        except Exception, e:
+            logger.error("Test Failed - ERROR Message:" + str(e))
+            self.assert_(False, case_name)
+        finally:
+            self.restore_environment()
+            logger.info("========== End of Running Test Case: %s ==========" % case_name)
 
-		#check the version in repo list
-		cmd = "subscription-manager repos --list|grep 'Repo Url'"
-		cmd510 = "subscription-manager repos --list|grep 'Repo URL'"
-		(ret,output)=eu().runcmd(session,cmd,"list available repos")
-
-		if ret == 0 and currentversion in output:      
-			logging.info("It's successful to display the set release version.")
-		else:
-			(ret,output)=eu().runcmd(session,cmd510,"list available repos")
-			if ret == 0 and currentversion in output:
-				logging.info("It's successful to display the set release version.")
-			else:
-				raise error.TestFail("Test Failed - Failed to display the set release version.")
-
-	except Exception, e:
-		logging.error(str(e))
-		raise error.TestFail("Test Failed - error happened when set release version:"+str(e))
-	finally:
-		eu().sub_unregister(session)
-		logging.info("=========== End of Running Test Case: %s ==========="%__name__)
+if __name__ == "__main__":
+    unittest.main()
