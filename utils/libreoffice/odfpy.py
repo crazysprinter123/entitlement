458b8271d4e1a61acfed7aaf1c2dd0a1497f8797
sgao <sgao@redhat.com>
2014-11-28 16:14:03 +0800

    read ods blank cells

--- utils/libreoffice/odfpy.py
+++ utils/libreoffice/odfpy.py
@@ -18,23 +18,33 @@ class ODSReader:
         # for each row
         for row in rows:
             cells = row.getElementsByType(TableCell)
-            # for each cell
-            for cell in cells:
-                textContent = ""
-                # repeated value?
-                repeat = cell.getAttribute("numbercolumnsrepeated")
-                if(not repeat):
-                    repeat = 1
-                ps = cell.getElementsByType(P)
-                for p in ps:
-                    for n in p.childNodes:
-                        if (n.nodeType == 3):
-                            textContent = unicode(n.data)
-                if(textContent):
-                    if(textContent[0] != "#"):  # ignore comments cells
-                        for rr in range(int(repeat)):  # repeated cells
-                            if textContent != "":
-                                print textContent
+            if len(cells) == 1:  # ignore blank rows
+                continue
+            else:
+                # for each cell
+                for cell in cells:
+                    textContent = ""
+                    # repeated value?
+                    repeat = cell.getAttribute("numbercolumnsrepeated")
+                    if(not repeat):
+                        repeat = 1
+                    ps = cell.getElementsByType(P)
+                    if len(ps) == 0:
+                        for rr in range(int(repeat)):  # repeated blank cells
+                            print "blank"
+                    else:
+                        for p in ps:
+                            for n in p.childNodes:
+                                if (n.nodeType == 3):
+                                    textContent = n.data
+#                                     textContent = unicode(n.data)
+                        if(textContent):
+                            if(textContent[0] != "#"):  # ignore comments cells
+                                for rr in range(int(repeat)):  # repeated cells
+                                    if textContent != "":
+                                        print textContent
 
 if __name__ == "__main__":
     ODSReader("test.ods")
+# improvement
+# 1, can not read blanks at the begin or end of cells
